38a2fb90e5a2edfab72aad578081ea26d6afdd98
Kuba Birecki <kuba.birecki@crystalplanet.io>
2021-05-09 12:35:10 +0200

    Add different toggle settings for the feedback button block

--- client/components/feedback/toggle.js
+++ client/components/feedback/toggle.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useCallback, useEffect } from 'react';
 import classnames from 'classnames';
 
 /**
@@ -14,6 +14,7 @@ import { RichText } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import CloseIcon from 'components/icon/close-small';
+import { FeedbackToggleMode } from 'blocks/feedback/constants';
 
 const CloseButton = () => (
 	<>
@@ -23,6 +24,22 @@ const CloseButton = () => (
 );
 
 const FeedbackToggle = ( { attributes, className, isOpen, onClick }, ref ) => {
+	useEffect( () => {
+		if ( isOpen || attributes.toggleOn !== FeedbackToggleMode.PAGE_LOAD ) {
+			return;
+		}
+
+		onClick();
+	}, [] );
+
+	const handleHover = useCallback( () => {
+		if ( isOpen || attributes.toggleOn !== FeedbackToggleMode.HOVER ) {
+			return;
+		}
+
+		onClick();
+	}, [ attributes.toggleOn, isOpen ] );
+
 	const classes = classnames(
 		'crowdsignal-forms-feedback__trigger',
 		'wp-block-button__link',
@@ -34,7 +51,12 @@ const FeedbackToggle = ( { attributes, className, isOpen, onClick }, ref ) => {
 
 	return (
 		<div className="wp-block-button crowdsignal-forms-feedback__trigger-wrapper">
-			<button ref={ ref } className={ classes } onClick={ onClick }>
+			<button
+				ref={ ref }
+				className={ classes }
+				onClick={ onClick }
+				onMouseEnter={ handleHover }
+			>
 				{ isOpen ? (
 					<CloseButton />
 				) : (
